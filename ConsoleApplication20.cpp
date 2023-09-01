a69851db42e04132c6b1560cec8c2a5c02a2fda6
Nikolay <Nikolay@git.com>
2023-09-01 20:26:56 +0300

    add Func sum array

--- ConsoleApplication20.cpp
+++ ConsoleApplication20.cpp
@@ -6,6 +6,14 @@ int sum(int a, int b) {
     return a + b;
 }
 
+int sum(int mas[], int size) {
+    int result = 0;
+    for (int i = 0; i < size; ++i)
+        result += mas[i];
+
+    return result;
+}
+
 int main()
 {
     cout << "Hello World!\n";
